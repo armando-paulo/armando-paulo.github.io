de024edae7cd18bc0dbb18c039d9acb3fc87103d
armandopaulo <aarmandoap01@gmail.com>
2025-10-08 16:49:14 +0000

    lab 4 inicio

--- lab4/scripts/main.js
+++ lab4/scripts/main.js
@@ -2,7 +2,10 @@
 
 let counter = 0;
 const heading = document.querySelector('h1');
+<button type="button" onclick= "window.alert('Olá')">
+   Alerta
 
+</button>
 function count() {
    counter++;
    heading.textContent = counter;
